7b888df0280c8944662a1469681d0f19f4c69e88
nmcanall <nmcanall@citadel.edu>
2022-01-16 22:17:47 -0600

    initial challenge worth saving

--- src/App.js
+++ src/App.js
@@ -9,9 +9,51 @@ function App() {
 }
 
 function MainContent() {
-    return (
-        <h1>Sup man</h1>
-    )
+  return (
+    <div>
+      <Header/>
+      <PageBody/>
+      <Footer/>
+    </div>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src={require("./logo192.png")} width="40px" alt="default React logo"/>
+      </nav>
+    </header>
+  )
+}
+
+function PageBody() {
+  return (
+    <header>
+      <h1>I love React</h1>
+      <MyList/>
+    </header>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      © 2022 McAnally development. All rights reserved.
+    </footer>
+  )
+}
+
+function MyList() {
+  return (
+    <ol>
+      <li>Absolutely nothing</li>
+      <li>But there's a lot in the background</li>
+      <li>MyCreateReact did it all for me</li>
+      <li>But I'll make it great soon</li>
+    </ol>
+  )
 }
 
 export default App;
